a92643bbe5c561257f419665881217de73fec0b7
Mateusz Widuch <matt.widuch@gmail.com>
2015-10-30 18:09:32 +0000

    Added fields for each button

--- app/src/main/java/com/redraysoftware/myappportfolio/MainActivity.java
+++ app/src/main/java/com/redraysoftware/myappportfolio/MainActivity.java
@@ -1,15 +1,20 @@
 package com.redraysoftware.myappportfolio;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mButtonMoviesApp;
+    private Button mButtonScoresApp;
+    private Button mButtonLibraryApp;
+    private Button mButtonBIBApp;
+    private Button mButtonReaderApp;
+    private Button mButtonCapstoneApp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
